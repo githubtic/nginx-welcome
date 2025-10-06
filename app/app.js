33172fa13f4b4821e90c9214a1cde01f643194a4
Steve Kabore <fabricestevekabore@gmail.com>
2025-10-06 16:38:19 +0000

    initial commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+app.get('/', (req, res) => res.send('Hello from Docker + Nginx + HTTPS!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
